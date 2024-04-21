765f721275a8343e55b06aeb8166243ed688a5a1
xmases <141183655+xmases@users.noreply.github.com>
2024-04-21 20:00:51 +0200

    Bug fixes & .gitignore

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import sys
 
-class cords:
-    def __init__(self, value) :
-        self.x, self.y = x, y
+cnt, argv = 0, []
+while cnt != 4:
+    cnt += 1
+    argv.append(int(sys.argv[cnt]))
 
-argv, cordsa, cordsb = cords(sys.argv[1:2]), cords(sys.argv[3:4])
+cordsa, cordsb = argv[0:2], argv[2:4]
 
 def froute(a, b, frames):
     defc, frate = [], []
     if not isinstance(a, list) and not isinstance (b, list) :
         assert False, "An error ocurred. Program terminated"
     
-    for i in range(1) :
+    for i in range(2) :
         defc.append(b[i] - a[i])
     
     for x in defc :
         frate.append(x / frames)
 
-
-
+    return defc, frate
